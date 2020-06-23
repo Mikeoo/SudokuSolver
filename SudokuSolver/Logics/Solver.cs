96b57da0efa058b80da74788198dd4064cedd00d
Stella van der burg <luna4s108@gmail.com>
2020-06-23 11:10:40 +0200

    - Added a (for now) empty nested for loop in a separate function to itirate through the blocks of the sudoku.

--- SudokuSolver/Logics/Solver.cs
+++ SudokuSolver/Logics/Solver.cs
@@ -10,7 +10,7 @@ namespace SudokuSolver.Logics
 {
     public class Solver
     {
-
+        private readonly int N = 3, N2 = 9;
         public int[][] Solve(int[][] sudoku)
         {
             return sudoku;
@@ -25,5 +25,29 @@ namespace SudokuSolver.Logics
         {
             return sudoku;
         }
+
+        #region Sudoku Dissection
+        private void DissectSudoku()
+        {
+            ///For Itirating through blocks.
+            /// i and j each go through N long steps. Checking a block N by N.
+            /// x adds x * N onto i to check the next two blocks over.
+            /// Whilst y adds y * N onto j each loop to start one block down after the first row of blocks was tested.
+            for (int y = 0; x < N; y++)
+            {
+                for (int x = 0; x < N; x++)
+                {
+                    for (int i = (0 + (x * N)); i < (N + (N * x)); i++)
+                    {
+                        for (int j = (0 + (y * N)); j < (N + (N * y)); j++)
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+
+        #endregion
     }
 }
